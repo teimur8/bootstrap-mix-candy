a576dcba09559cc3b9031307627c01f450ef092a
Timur Gaisin <it05@adkulan.kz>
2019-03-18 15:44:26 +0600

    done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 require("babel-polyfill");
 window.$ = window.jQuery = require("jquery");
 import "bootstrap";
+
+$(function() {
+  $(document).scroll(function() {
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
